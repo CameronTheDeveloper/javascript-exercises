7b83affe057b0472d5b892b9ce1efe3162a002ee
CameronTheDeveloper <cameronposey@outlook.com>
2023-01-29 18:22:59 -0800

    Complete sections 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word,j) {
+    let i = 0;
+    let repeatedWord = "";
+    while (i < j){
+        repeatedWord += word;
+        i++;
+    }
+    return repeatedWord;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...j) {
+   let newArr = [];
 
+   arr.forEach((item) => {
+    if (!j.includes(item)){
+        newArr.push(item);
+    }
+   })
+   return newArr;
 };
 
 // Do not edit below this line
